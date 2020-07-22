a01dc46e89f638c3b5774dc69a581c49c51ebad1
rajkharvar <rajkharvar1808@gmail.com>
2020-07-22 17:53:15 +0530

    🔌 css minify and tailwindcss via node

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./public/index.html", "./public/main.js"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
